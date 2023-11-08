d6fc7db9812d15e7d3ea977a197a65745ecfdddc
Histalek <16392835+Histalek@users.noreply.github.com>
2023-11-08 20:10:47 +0100

    refactor: Avoid some unwraps, remove useless else arms

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+use anyhow::Result;
 use askama::Template;
 use glob::glob;
 use serde_json::Value;
 use std::env;
 use std::fs;
 use std::fs::File;
-use std::io::BufReader;
-use std::io::{BufWriter, Write};
+use std::io::{BufReader, BufWriter, Write};
 use std::path::PathBuf;
-use std::str::FromStr;
-use tracing::info;
+use tracing::{error, info};
 
 mod neodoc_parser;
 use neodoc_parser::*;
@@ -24,9 +23,8 @@ fn main() {
     let output_dir = PathBuf::from(&args[2])
         .canonicalize()
         .expect("Could not get canonicalized path for output directory. Does the directory exist?")
-        .into_os_string()
-        .into_string()
-        .unwrap();
+        .to_string_lossy()
+        .to_string();
 
     for wrapper in fs::read_dir(input_dir).unwrap() {
         let wrapper = wrapper.unwrap().path();
@@ -34,10 +32,8 @@ fn main() {
             for section in fs::read_dir(&wrapper).unwrap() {
                 let section = section.unwrap().path();
                 if section.is_dir() {
-                    let detail_json_path = PathBuf::from_str(
-                        format!("{}/detail.json", section.to_string_lossy()).as_str(),
-                    )
-                    .unwrap();
+                    let detail_json_path =
+                        PathBuf::from(format!("{}/detail.json", section.to_string_lossy()));
                     let buf_reader = BufReader::new(
                         File::open(detail_json_path).expect("Error while openening detail.json"),
                     );
@@ -64,44 +60,43 @@ fn main() {
                             {
                                 match neodoc_func {
                                     Ok(path) => {
-                                        info!("Reading {:?}", path);
-                                        match read_neodoc_json(path) {
-                                            Some(function) => lua_definition
-                                                .functions
-                                                .push(neodoc_to_func_definition(function)),
-                                            None => continue,
-                                        };
-                                        // let function = read_neodoc_json(File::open(&path).unwrap());
+                                        if path.file_name().is_some_and(|x| x.ne("detail.json")) {
+                                            info!("Reading {:?}", path);
+                                            match read_neodoc_json(path) {
+                                                Some(function) => lua_definition
+                                                    .functions
+                                                    .push(neodoc_to_func_definition(function)),
+                                                None => continue,
+                                            };
+                                        }
                                     }
-                                    Err(e) => println!("{:?}", e),
+                                    Err(e) => error!("{:?}", e),
                                 }
                             }
-                            let file = File::create(format!(
+                            let filepath = format!(
                                 "{}/{}.lua",
                                 &output_dir,
-                                section.file_name().unwrap().to_str().unwrap(),
-                            ))
-                            .unwrap();
-                            render_definition_file(file, lua_definition);
-                        } else {
-                            continue;
+                                section.file_name().unwrap().to_string_lossy()
+                            );
+                            let file = File::create(&filepath).unwrap();
+                            match render_definition_file(file, lua_definition) {
+                                Ok(()) => continue,
+                                Err(e) => error!("Error writing {} : {}", &filepath, e),
+                            }
                         }
                     }
-                } else {
-                    continue;
                 }
             }
-        } else {
-            continue;
         }
     }
 }
 
-fn render_definition_file(file: File, lua_def: LuaModuleFile) {
+fn render_definition_file(file: File, lua_def: LuaModuleFile) -> Result<()> {
     let mut buf_writer = BufWriter::new(file);
 
-    let template_str = lua_def.render().unwrap();
-    write!(buf_writer, "{}", &template_str).unwrap();
+    let template_str = lua_def.render()?;
+    write!(buf_writer, "{}", &template_str)?;
 
-    buf_writer.flush().unwrap();
+    buf_writer.flush()?;
+    Ok(())
 }
